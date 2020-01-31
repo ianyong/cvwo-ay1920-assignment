5955ef8495369e9be89c8074253056d8952ac881
Ian Yong <ianyongyc@gmail.com>
2020-01-31 18:33:45 +0800

    Add edit button to view task dialog

--- src/app/javascript/components/TaskDetailsDialog.js
+++ src/app/javascript/components/TaskDetailsDialog.js
@@ -1,35 +1,64 @@
 import React from "react";
 import { Dialog, DialogTitle, DialogActions, Button, DialogContent, DialogContentText } from "@material-ui/core";
+import AddUpdateTaskDialog from "./AddUpdateTaskDialog";
 
 class TaskDetailsDialog extends React.Component {
   constructor(props) {
     super(props);
+    this.openDialog = this.openDialog.bind(this);
+    this.closeDialog = this.closeDialog.bind(this);
+    this.state = {
+      dialogOpen: false
+    };
+  }
+
+  openDialog() {
+    this.setState({
+      dialogOpen: true
+    });
+  }
+
+  closeDialog() {
+    this.setState({
+      dialogOpen: false
+    });
   }
   
   render() {
     return (
-      <Dialog
-        open={this.props.open}
-        fullWidth={true}
-        maxWidth="md"
-        scroll="paper">
-        <DialogTitle>
-          {this.props.task.attributes.name}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            {this.props.task.attributes.description}
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button
-            autoFocus
-            onClick={this.props.onClose}
-            color="primary">
-            Close
-          </Button>
-        </DialogActions>
-      </Dialog>
+      <React.Fragment>
+        <Dialog
+          open={this.props.open}
+          fullWidth={true}
+          maxWidth="md"
+          scroll="paper">
+          <DialogTitle>
+            {this.props.task.attributes.name}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              {this.props.task.attributes.description}
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button
+              onClick={() => { this.props.onClose(); this.openDialog(); }}
+              color="secondary">
+              Edit
+            </Button>
+            <Button
+              autoFocus
+              onClick={this.props.onClose}
+              color="primary">
+              Close
+            </Button>
+          </DialogActions>
+        </Dialog>
+        <AddUpdateTaskDialog
+          task={this.props.task}
+          open={this.state.dialogOpen}
+          onClose={this.closeDialog} />
+      </React.Fragment>
     );
   }
 }
