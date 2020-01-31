313bd49a9fa01ed2fc8895c607fee185477dde77
Ian Yong <ianyongyc@gmail.com>
2020-01-31 14:50:52 +0800

    Add tag and tagging models to database

--- src/app/models/task.rb
+++ src/app/models/task.rb
@@ -1,4 +1,24 @@
 class Task < ApplicationRecord
   # Associations
   belongs_to :user
+  has_many :taggings
+  has_many :tags, through: :taggings
+
+  def self.tagged_with(name)
+    Tag.find_by!(name: name).tasks
+  end
+
+  def self.tagged_counts
+    Tag.select('tags.*, count(taggings.tag_id) as count').joins(:taggings).group('taggings.tag_id')
+  end
+
+  def tag_list
+    tags.map(&:name).join(', ')
+  end
+
+  def tag_list=(names)
+    self.tags = names.split(',').map do |n|
+      Tag.where(name: n.strip).first_or_create!
+    end
+  end
 end
