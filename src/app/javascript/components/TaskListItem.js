71fca96dbafa209201c6887764bef81593f1d6ea
Ian Yong <ianyongyc@gmail.com>
2020-02-03 23:08:12 +0800

    Sort tags before displaying
    Resolves #14

--- src/app/javascript/components/TaskListItem.js
+++ src/app/javascript/components/TaskListItem.js
@@ -95,7 +95,7 @@ class TaskListItem extends React.Component {
                 label={moment(this.props.task.attributes['due-date']).format('D MMMM YYYY')}
                 variant="outlined" />
               <div>
-                {this.props.task.attributes['tag-list'].split("\u0000").filter(e => e !== "").map((tag, index) => {
+                {this.props.task.attributes['tag-list'].split("\u0000").sort().filter(e => e !== "").map((tag, index) => {
                   return(
                     <Chip
                       className="tag cursor-pointer"
