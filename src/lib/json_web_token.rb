410d86b4294fe866057e93a51fff9dbb050260d0
Ian Yong <ianyongyc@gmail.com>
2020-02-07 18:34:50 +0800

    Use credentials to encode

--- src/lib/json_web_token.rb
+++ src/lib/json_web_token.rb
@@ -5,12 +5,12 @@ class JsonWebToken
       payload[:exp] = exp.to_i
 
       # Encode the user data (payload) with secret key
-      JWT.encode(payload, Rails.application.secrets.secret_key_base)
+      JWT.encode(payload, Rails.application.credentials.secret_key_base)
     end
 
     def decode(token)
       # Decode the token to get the user data (payload) with secret key
-      body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+      body = JWT.decode(token, Rails.application.credentials.secret_key_base)[0]
       HashWithIndifferentAccess.new body
 
     # Raise custom error
